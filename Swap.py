95db789e15156ff4c7bd802fe4b1aa1ae0a0e464
Dhriti Dogra <83918429+22dhriti01@users.noreply.github.com>
2021-06-08 11:02:28 +0530

    Add files via upload

---
+++ Swap.py
@@ -0,0 +1,12 @@
+Swap Praogram:
+
+x = input('Enter value of x: ')  
+y = input('Enter value of y: ')  
+  
+# create a temporary variable and swap the values  
+temp = x  
+x = y  
+y = temp  
+  
+print("Value of x:", x)
+print("Value of y:", y)
---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+Fibonacci Program:
+
+a = 0
+b = 1
+
+print(a)
+print(b)
+while(a <= 100):
+    print(a)
+    c = a
+    a = a + b
+    b = c
